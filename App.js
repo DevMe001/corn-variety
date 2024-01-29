bc8a5dd465a61bf4300f5a1dd0d06efc419259f3
DevMe001 <dev.me28@gmail.com>
2024-01-29 15:12:45 +0800

    add requirement.txt

--- App.js
+++ App.js
@@ -84,27 +84,50 @@ export default function App() {
   }
 
   return (
-    <Container style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-      {/* <Text style={{ fontSize: '2.25rem', textAlign: 'center', fontWeight: '700', color: '#222423' }}>CORN VARIETY</Text> */}
-      <Pressable onPress={getCornVariety} ><Text style={{ fontSize: 12, color: '#a1a1a1' }}>about this app</Text></Pressable>
-      <Spacer size={6} />
-      <LogoBox />
-      <Spacer size={24} />
-      <View style={{ width: 250 }} >
-        <View style={{ display: 'flex', gap: 1, flexDirection: 'column' }}>
-          <Pressable style={{ padding: 10, borderColor: '#88d450', borderStyle: 'solid', borderWidth: 3, maxWidth: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: 8, flexGrow: 1 }} onPress={pickImage}>
-            <Entypo name="upload" size={64} color="#88d450" />
-            <Text style={{ textTransform: 'uppercase', fontSize: 10, fontWeight: 600, color: '#303030' }}>Upload an Image</Text>
-          </Pressable>
-          <Pressable style={{ padding: 10, borderColor: '#f37521', borderStyle: 'solid', borderWidth: 3, maxWidth: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: 8, flexGrow: 1 }} onPress={openCamera}>
-            <AntDesign name="camera" size={64} color="#f37521" />
-            <Text style={{ textTransform: 'uppercase', fontSize: 10, fontWeight: 600, color: '#303030' }}>Capture an Image</Text>
-          </Pressable>
-        </View>
-        <Spacer size={16} />
-        
-        <Text style={{ textTransform: 'uppercase', fontSize: 12, fontWeight: '600', color: '#303030' }}>Variety: {varietyName}</Text>
-      </View>
-    </Container>
-  );
+		<Container style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+			{/* <Text style={{ fontSize: '2.25rem', textAlign: 'center', fontWeight: '700', color: '#222423' }}>CORN VARIETY</Text> */}
+			<Pressable onPress={getCornVariety}>
+				<Text style={{ fontSize: 12, color: '#a1a1a1' }}>about this app</Text>
+			</Pressable>
+			<Spacer size={6} />
+			<LogoBox />
+			<Spacer size={24} />
+			<View style={{ width: 250 }}>
+				<View style={{ display: 'flex', gap: 1, flexDirection: 'column' }}>
+					<Pressable style={{ padding: 10, borderColor: '#88d450', borderStyle: 'solid', borderWidth: 3, maxWidth: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: 8, flexGrow: 1 }} onPress={pickImage}>
+						<Entypo name='upload' size={64} color='#88d450' />
+						<Text style={{ textTransform: 'uppercase', fontSize: 10, fontWeight: 600, color: '#303030' }}>Upload an Image</Text>
+					</Pressable>
+					<Pressable style={{ padding: 10, borderColor: '#f37521', borderStyle: 'solid', borderWidth: 3, maxWidth: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: 8, flexGrow: 1 }} onPress={openCamera}>
+						<AntDesign name='camera' size={64} color='#f37521' />
+						<Text style={{ textTransform: 'uppercase', fontSize: 10, fontWeight: 600, color: '#303030' }}>Capture an Image</Text>
+					</Pressable>
+				</View>
+				<Spacer size={16} />
+
+				{varietyName && (
+					<>
+						<Text style={{ textTransform: 'uppercase', fontSize: 12, fontWeight: '600', color: '#303030' }}>Variety: {varietyName}</Text>
+					</>
+				)}
+			</View>
+
+			<View style={styles.container}>
+				<Image source={image} style={styles.image} />
+			</View>
+		</Container>
+	);
 }
+
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	image: {
+		width: 300, // Set the width here
+		height: 300, // Set the height here
+	},
+});
